dcf8c31763d163562bd731c697af0a8665be3cfa
Takafumi Umemoto <takafumi.umemoto@gmail.com>
2019-04-28 02:17:37 +0900

    引数をランダムに出力するchoiceを追加

---
+++ choice/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	c := len(os.Args) - 1
+	if c < 1 {
+		fmt.Fprintf(os.Stderr, "[usage] %s choice1 chioce2...,", os.Args[0])
+		os.Exit(1)
+	}
+	fmt.Printf(os.Args[rand.Intn(c)+1])
+}
